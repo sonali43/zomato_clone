c850cbff3d4ceef940477c8aac0f161e9a94bcba
sonali43 <sonalisethi439@gmail.com>
2026-08-22 18:09:51 +0530

    nothing

--- app/security/auth_service.py
+++ app/security/auth_service.py
@@ -5,3 +5,20 @@ from fastapi import HTTPException,status
 import jwt
 from app.core.logger_config import logging
 
+SECRET_KEY = 'sonali123'
+ALGORITHM = 'HS256'
+
+def verify_access_token(self,token):
+    auth=AuthContext
+    payload = decode_token(token)
+    user = self.user_repo.get_user_by_id(payload["user_id"])
+    
+    if not user:
+        raise UserNotFound()
+    
+    auth = AuthContext(
+        user_id = user.id,
+        permissions=permissions,
+    )
+    
+    raise 
