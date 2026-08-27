e55cd7b4c8df411252bc94a38050dd5d3cb01a00
sonali43 <sonalisethi439@gmail.com>
2026-08-27 21:51:35 +0530

    today change in auth

--- app/security/auth_service.py
+++ app/security/auth_service.py
@@ -23,3 +23,26 @@ def verify_access_token(self,token):
     
     raise verify_access_token
 
+def generate_token(user_id :int):
+    user_id = str(user_id)
+    payload = {
+        "user_id" : user_id,
+        "exp":datetime.datetime.now(timezone.utc) + datetime.timedelta(minutes=60),
+    }
+    
+    token = jwt.encode(payload,SECRET_KEY,algorithm=ALGORITHM)
+    logger.info(f"user with {user_id} has this {token}")
+    return token
+
+def decode_auth_token(auth_token : str):
+    try:
+        payload = jwt.decode(auth_token,
+                             SECRET_KEY,
+                             algorithms=ALGORITHM)
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    except jwt.PyJWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    
+    logger.info(f"user with {auth_token} has this {payload}")
+    return payload
