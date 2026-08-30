aded0b392f0d33b6134c9030a5736d0b3919cfb0
sonali43 <sonalisethi439@gmail.com>
2026-08-30 16:53:14 +0530

    slit changes in auth service

--- app/security/auth_service.py
+++ app/security/auth_service.py
@@ -1,26 +1,9 @@
 import datetime
 from datetime import timezone
+from app.repo.user_repo import UserRepository
+from app.custom_exception.custom_exception import UserNotFoundException
 
-from fastapi import HTTPException,status
-import jwt
-from app.core.logger_config import logging
-
-SECRET_KEY = 'sonali123'
-ALGORITHM = 'HS256'
-
-def verify_access_token(self,token):
-    auth=AuthContext
-    payload = decode_token(token)
-    user = self.user_repo.get_user_by_id(payload["user_id"])
+class AuthService:
     
-    if not user:
-        raise UserNotFound()
-    
-    auth = AuthContext(
-        user_id = user.id,
-        permissions=permissions,
-    )
-    
-    raise verify_access_token
 
 
