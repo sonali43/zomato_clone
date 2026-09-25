aee295410deb66ea7f15bc4a8008a1497f19597e
sonali43 <sonalisethi439@gmail.com>
2026-09-25 18:25:15 +0530

    address repo completed

--- app/repo/address_repo.py
+++ app/repo/address_repo.py
@@ -35,9 +35,34 @@ class AddressRepository:
         address=self.db_session.query(Address).filter(Address.user_id==user_id).all()
         return address
     
-    def update_address(self,address_id:int,address:UpdateAddress):
-        existing_address=self.db_session.query(Address).filter(Address.id==address_id).first()
-        if existing_address is None:
-            return None
-        if existing_address is not None:
+    def update_address(self,address:Address,update_address:UpdateAddress):
+        if update_address.house_no is not None:
+            address.house_no= update_address.house_no  
+        if update_address.street is not None:
+            address.street = update_address.street
+        if update_address.city is not None:
+            address.city = update_address.city
+        if update_address.state is not None:
+            address.state = update_address.state
+        if update_address.pincode is not None:
+            address.pincode = update_address.pincode
+        if update_address.landmark is not None:
+            address.landmark = update_address.landmark
+        if update_address.address_type is not None:
+            address.address_type = update_address.address_type
             
+        try:
+            self.db_session.commit()
+            self.db_session.refresh(address)
+            return address
+        except Exception as e:
+            self.db_session.rollback()
+            raise ValueError(str(e))
+        
+    def delete_address(self,address:Address):
+        try:
+            self.db_session.delete(address)
+            self.db_session.commit()
+        except Exception as e:
+            self.db_session.rollback()
+            raise ValueError (str(e))
